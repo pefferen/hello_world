c18d93002c1aeee0f4a4e10ad914a9aba0169328
Patrick van Efferen <patrick@triquanta.nl>
2015-04-14 17:38:21 +0200

    Added Configuration to block.

--- src/Plugin/Block/HelloBlock.php
+++ src/Plugin/Block/HelloBlock.php
@@ -3,6 +3,8 @@
 namespace Drupal\hello_world\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Display's our Hello World block.
@@ -20,6 +22,30 @@ class HelloBlock extends BlockBase {
     return array(
       '#markup' => $this->t('Hello World'),
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $config = $this->getConfiguration();
+
+    $form['hello_block_name'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Who'),
+      '#description' => $this->t('Who do you want to say hello to?'),
+      '#default_value' => isset($config['name']) ? $config['name'] : '',
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('name', $form_state->getValue('hello_block_name'));
   }
 
 }
